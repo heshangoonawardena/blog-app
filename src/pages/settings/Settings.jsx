0be445549aba369b30d3d3347feb5f83fd48159c
heshangoonawardena <heshangoonawardena@gmail.com>
2024-10-09 16:11:32 +0530

    create ⚙️settings page

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,40 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import "./Settings.css";
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <FaRegUserCircle className="settingsPPIcon" />
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Heshan" />
+          <label>Email</label>
+          <input type="email" placeholder="Heshan@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Settings;
