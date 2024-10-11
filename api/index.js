eb5845b33128c66716e2d9844f0974edb2b0cf70
heshangoonawardena <heshangoonawardena@gmail.com>
2024-10-11 18:25:06 +0530

    Init db connection n add env variables

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(console.log('connected to MongoDB')).catch((e) => console.log(e));
 
 
 app.listen("5000", () => {
