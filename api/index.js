f430ac704f8ed823522317797eedf313a9225bb3
heshangoonawardena <heshangoonawardena@gmail.com>
2024-10-13 15:00:46 +0530

    Create user authentication

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(console.log('connected to MongoDB')).catch((e) => console.log(e));
+app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(console.log('connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen("5000", () => {
   console.log("Server is running on port 5000");
